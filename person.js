e315aeb51bda9e7f4d7b78fefe38d1ee5c5e56b1
Obaida <15270128+TA4life@users.noreply.github.com>
2019-12-01 16:44:57 -0500

    Add files via upload

--- person.js
+++ person.js
@@ -1,6 +1,6 @@
 function Person(y,m) {
   this.pos = createVector(50+y, height-30-y*10);
-  this.vel = createVector(1, 0);
+  this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
   this.mass = 0;
 
@@ -9,13 +9,31 @@ function Person(y,m) {
     // f.div(this.mass);
     this.acc.add(force);
   }
-
+  
+  this.moveRight = function(){
+    this.vel.sub(this.vel.copy());
+    this.vel.add(2)
+  }
+  
+  this.moveLeft = function(){
+    this.vel.sub(this.vel.copy());
+    this.vel.add(-2)
+  }
+  
+  this.stop = function(){
+    this.vel = createVector(0,0);
+  }
+  
+  this.onGround = function(){
+    return this.vel.y == 0;
+  }
+  
   this.update = function() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
   }
-
+  
   this.display = function() {
     fill(30);
     rect(this.pos.x, this.pos.y, 30, 30);
